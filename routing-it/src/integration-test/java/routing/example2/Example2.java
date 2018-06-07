daf5a6bab629bfe0af9453278b5c0c855f4f041b
Josh Long <starbuxman@gmail.com>
2018-06-07 01:51:58 -0700

    adding BOM module.

--- routing-it/src/integration-test/java/routing/example2/Example2.java
+++ routing-it/src/integration-test/java/routing/example2/Example2.java
@@ -15,6 +15,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
