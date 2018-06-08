935aeef907b6d9abb72d05fcdfa5fa649712bace
Josh Long <starbuxman@gmail.com>
2018-06-07 18:40:27 -0700

    polish

--- routing-it/src/integration-test/java/routing/RoutingIT.java
+++ routing-it/src/integration-test/java/routing/RoutingIT.java
@@ -1,272 +1,227 @@
 package routing;
 
-//import cnj.CloudFoundryService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.cloudfoundry.client.v2.ClientV2Exception;
 import org.cloudfoundry.operations.CloudFoundryOperations;
-import org.cloudfoundry.operations.applications.ApplicationManifest;
+import org.cloudfoundry.operations.applications.*;
+import org.cloudfoundry.operations.routes.DeleteOrphanedRoutesRequest;
 import org.cloudfoundry.operations.routes.ListRoutesRequest;
 import org.cloudfoundry.operations.routes.Route;
 import org.cloudfoundry.operations.services.BindRouteServiceInstanceRequest;
 import org.cloudfoundry.operations.services.CreateUserProvidedServiceInstanceRequest;
+import org.cloudfoundry.operations.services.DeleteServiceInstanceRequest;
 import org.cloudfoundry.operations.services.UnbindRouteServiceInstanceRequest;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.NestedExceptionUtils;
-import org.springframework.http.ResponseEntity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-///*
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = RoutingIT.Config.class)
-//public class RoutingIT {
-//
-// @SpringBootApplication
-// public static class Config {
-// }
-//
-// private Log log = LogFactory.getLog(getClass());
-//
-// private File downstreamApplicationManifest, eurekaServiceManifest,
-//  routeServiceManifest;
-//
-// private String eurekaServiceId = "routing-eureka-service",
-//  downstreamServiceId = "downstream-service", routeServiceId = "route-service";
-//
-// private String routeServiceIdCups = routeServiceId + "-svc";
-//
-// @Autowired
-// private CloudFoundryOperations cloudFoundryOperations;
-//
-// private void applyRouteService() {
-//  log.info("binding route service for routeServiceId: " + routeServiceId
-//   + " and downstreamApplicationId: " + downstreamServiceId);
-//  String url = cf.urlForApplication(routeServiceId);
-//  if (url.contains("://")) {
-//   url = "https://" + url.split("://")[1];
-//  }
-//
-//  this.cloudFoundryOperations
-//   .services()
-//   .createUserProvidedInstance(
-//    CreateUserProvidedServiceInstanceRequest.builder().name(routeServiceIdCups)
-//     .routeServiceUrl(url).build()).block();
-//  Route first = forApplicationName(downstreamServiceId);
-//
-//  Assert.assertNotNull("there must be a route assigned to the service ID "
-//   + downstreamServiceId, first);
-//
-//  log.info("trying to apply route service with: " + "domain: "
-//   + first.getDomain() + ", path: " + first.getPath() + ", hostname: "
-//   + first.getHost() + ", routeServiceInstanceId: " + routeServiceIdCups);
-//
-//  this.cloudFoundryOperations
-//   .services()
-//   .bindRoute(
-//    BindRouteServiceInstanceRequest.builder()
-//     .serviceInstanceName(routeServiceIdCups).domainName(first.getDomain())
-//     .hostname(first.getHost()).path(first.getPath()).build()).block();
-//  this.log.info("done!");
-// }
-//
-// private void deleteApps() throws Throwable {
-//  Stream
-//   .of(this.routeServiceId, this.downstreamServiceId, this.eurekaServiceId)
-//   .forEach(a -> {
-//    log.info("app: " + a);
-//    this.cf.destroyApplicationIfExists(a);
-//   });
-// }
-//
-// private void reset() throws Throwable {
-//
-//  // delete apps
-//
-//  // unbind route service
-//
-//  /*
-//   * Route downstreamAppRoute =
-//   * this.forApplicationName
-//   * (this.routeServiceId); if (null !=
-//   * downstreamAppRoute &&
-//   * cf.applicationExists
-//   * (this.downstreamServiceId)) { try {
-//   * log.info(String.format(
-//   * "attempting to unbind route service for host %s and domain %s and service-instance %s"
-//   * , downstreamAppRoute.getHost(),
-//   * downstreamAppRoute.getDomain(),
-//   * routeServiceId));
-//   *
-//   * cloudFoundryOperations.services()
-//   * .unbindRoute
-//   * (UnbindRouteServiceInstanceRequest
-//   * .builder()
-//   * .serviceInstanceName(routeServiceIdCups
-//   * ).domainName(downstreamAppRoute.
-//   * getDomain())
-//   * .hostname(downstreamAppRoute
-//   * .getHost
-//   * ()).path(downstreamAppRoute.
-//   * getPath()).build()) .block(); }
-//   * catch (Throwable t) {
-//   * log.warn("oops!", t); }
-//   *
-//   * }
-//   */
-//
-//  // ///////////////////////////
-//
-//  // cf.destroyServiceIfExists(routeServiceIdCups);
-//
-//  Route first = forApplicationName(downstreamServiceId);
-//
-//  // deleteApps();
-//
-//  if (null != first) {
-//
-//   try {
-//    log.info("trying to unbind route service with: " + "domain: "
-//     + first.getDomain() + ", path: " + first.getPath() + ", hostname: "
-//     + first.getHost() + ", routeServiceInstanceId: " + routeServiceIdCups);
-//
-//    this.cloudFoundryOperations
-//     .services()
-//     .unbindRoute(
-//      UnbindRouteServiceInstanceRequest.builder()
-//       .serviceInstanceName(routeServiceIdCups).domainName(first.getDomain())
-//       .hostname(first.getHost()).path(first.getPath()).build()).block();
-//   }
-//   catch (Exception e) {
-//    this.log.warn(NestedExceptionUtils.buildMessage(e.getMessage(), e));
-//   }
-//   this.log.info("done trying to unbind..");
-//  }
-//
-//  // //////////////////////////
-//
-//  // delete orphaned routes
-//  cf.destroyOrphanedRoutes();
-//
-//  this.deleteApps();
-//
-//  // delete services
-//  String[] svcs = "route-service-svc,routing-eureka-service".split(",");
-//  for (String s : svcs) {
-//   log.info("service: " + s);
-//   try {
-//    this.cf.destroyServiceIfExists(s);
-//   }
-//   catch (ClientV2Exception e) {
-//    this.log.info("exception: '" + e.getDescription()
-//     + "' on attempting to delete " + s);
-//   }
-//  }
-// }
-//
-// @Autowired
-// private CloudFoundryService cf;
-//
-// @Before
-// public void before() throws Throwable {
-//  File root = new File(".");
-//  this.downstreamApplicationManifest = new File(root,
-//   "../downstream-service/manifest.yml");
-//  this.eurekaServiceManifest = new File(root,
-//   "../routing-eureka-service/manifest.yml");
-//  this.routeServiceManifest = new File("../route-service/manifest.yml");
-//
-//  Stream.of(this.downstreamApplicationManifest, this.eurekaServiceManifest,
-//   this.routeServiceManifest).forEach(
-//   manifest -> Assert.assertTrue(manifest.getAbsolutePath() + " must exist",
-//    manifest.exists()));
-//
-//  this.reset();
-// }
-//
-// @After
-// public void after() throws Throwable {
-//  this.reset();
-// }
-//
-// @Test
-// public void routeService() throws Throwable {
-//
-//  // if (true) return;
-//
-//  this.deployEureka();
-//  this.deployDownstreamApplication();
-//  this.deployRouteServiceApplication();
-//  this.applyRouteService();
-//  RestTemplate restTemplate = new RestTemplateBuilder().build();
-//  ResponseEntity<String> responseEntity = restTemplate.getForEntity(
-//   this.cf.urlForApplication(downstreamServiceId) + "/hi/world", String.class);
-//  String body = responseEntity.getBody();
-//  log.info("response: " + body);
-//  Assert.assertTrue("the request should have been intercepted", body
-//   .toLowerCase().contains("Cloud Natives".toLowerCase()));
-// }
-//
-// private void deployEureka() throws Throwable {
-//  cf
-//   .applicationManifestFrom(this.eurekaServiceManifest)
-//   .entrySet()
-//   .stream()
-//   .map(
-//    e -> {
-//     ApplicationManifest manifest = e.getValue();
-//     String appName = manifest.getName();
-//     File manifestFile = e.getKey();
-//     this.cf.pushApplicationAndCreateUserDefinedServiceUsingManifest(
-//      manifestFile, manifest);
-//     return appName;
-//    })
-//   .findAny()
-//   .orElseThrow(
-//    () -> new RuntimeException(
-//     "couldn't deploy the Eureka application instance!"));
-// }
-//
-// private void deployRouteServiceApplication() {
-//  cf.applicationManifestFrom(this.routeServiceManifest).entrySet().stream()
-//   .map((e) -> {
-//    ApplicationManifest am = e.getValue();
-//    this.cf.pushApplicationUsingManifest(this.routeServiceManifest);
-//    return am.getName();
-//   }).findAny()
-//   .orElseThrow(() -> new RuntimeException("can't deploy the route service!"));
-// }
-//
-// private void deployDownstreamApplication() {
-//  cf
-//   .applicationManifestFrom(this.downstreamApplicationManifest)
-//   .entrySet()
-//   .stream()
-//   .map(e -> {
-//    File f = e.getKey();
-//    ApplicationManifest am = e.getValue();
-//    this.cf.pushApplicationUsingManifest(f, am, true);
-//    return am.getName();
-//   })
-//   .findAny()
-//   .orElseThrow(
-//    () -> new RuntimeException("couldn't deploy the downstream application!"));
-// }
-//
-// private Route forApplicationName(String appName) {
-//  return this.cloudFoundryOperations.routes()
-//   .list(ListRoutesRequest.builder().build())
-//   .filter(r -> r.getApplications().contains(appName)).blockFirst();
-// }
-//}
-// */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RoutingIT.Config.class)
+public class RoutingIT {
+
+		@SpringBootApplication
+		public static class Config {
+
+				@Bean
+				RouteServiceDeployer routeServiceDeployer(CloudFoundryService cfs, CloudFoundryOperations cops) {
+						return new RouteServiceDeployer(cfs, cops);
+				}
+
+				@Bean
+				CloudFoundryService cfs(CloudFoundryOperations ops) {
+						return new CloudFoundryService(ops);
+				}
+		}
+
+		@Autowired
+		private RouteServiceDeployer routeServiceDeployer;
+
+		@Test
+		public void deploy() {
+				StepVerifier
+					.create(this.routeServiceDeployer.deploy())
+					.expectNextCount(1)
+					.expectNext("route-service")
+					.verifyComplete();
+		}
+}
+
+class RouteServiceDeployer {
+
+		private final Log log = LogFactory.getLog(getClass());
+
+		private final File root = new File(".");
+		private final File routingEurekaServiceManifest = new File(this.root, "routing-eureka-service/manifest.yml");
+		private final File downstreamServiceManifest = new File(this.root, "downstream-service/manifest.yml");
+		private final File routeServiceManifest = new File(this.root, "route-service/manifest.yml");
+
+		private final Map<File, ApplicationManifest> manifests;
+		private final Map<File, String> applicationNames;
+
+		private final CloudFoundryOperations cloudfoundry;
+		private final CloudFoundryService cloudFoundryService;
+
+		public RouteServiceDeployer(CloudFoundryService cloudFoundryService, CloudFoundryOperations cloudfoundry) {
+				this.cloudFoundryService = cloudFoundryService;
+
+				this.manifests = Stream
+					.of(this.downstreamServiceManifest, this.routeServiceManifest, this.routingEurekaServiceManifest)
+					.collect(Collectors.toConcurrentMap(x -> x, x -> cloudFoundryService.getManifestFor(x.toPath())));
+
+				this.cloudfoundry = cloudfoundry;
+
+				this.applicationNames = this.manifests
+					.entrySet()
+					.stream()
+					.collect(
+						Collectors.toConcurrentMap(Map.Entry::getKey, x -> x.getValue().getName()));
+		}
+
+		private void error(String msg, Throwable throwable) {
+				log.warn("oh the humanity! " + msg);
+				if (null != throwable) {
+						log.error(throwable.getMessage());
+				}
+		}
+
+		public Flux<String> deploy() {
+
+				// unbind route service if it exists
+				String routeServiceAppName = this.manifests.get(this.routeServiceManifest).getName();
+				String downstreamServiceAppName = this.manifests.get(this.downstreamServiceManifest).getName();
+
+				Flux<String> unbindRouteService = cloudFoundryService
+					.findRoutesForApplication(downstreamServiceAppName)
+					.flatMap(route ->
+						cloudfoundry
+							.services()
+							.unbindRoute(UnbindRouteServiceInstanceRequest
+								.builder()
+								.serviceInstanceName(routeServiceAppName)
+								.domainName(route.getDomain())
+								.hostname(route.getHost())
+								.path(route.getPath())
+								.build())
+							.thenMany(Mono.just(routeServiceAppName))
+					)
+					.doOnError(ex -> error("something went wrong in unbinding the route service " + routeServiceAppName, ex))
+					.doOnComplete(() -> log.info("unbound route service " + routeServiceAppName));
+
+				Flux<String> deleteApplications = Flux
+					.just(this.downstreamServiceManifest, this.routeServiceManifest, this.routingEurekaServiceManifest)
+					.map(this.applicationNames::get)
+					.flatMap(name -> cloudfoundry
+						.applications()
+						.delete(DeleteApplicationRequest.builder().name(name).build())
+						.onErrorResume(IllegalArgumentException.class, ex -> {
+								error(String.format("can't delete application %s", name), ex);
+								return Mono.empty();
+						})
+						.then(Mono.just(name)))
+					.doOnComplete(() -> Stream
+						.of(this.downstreamServiceManifest, this.routeServiceManifest, this.routingEurekaServiceManifest)
+						.map(this.manifests::get)
+						.forEach(m -> log.info("deleted application " + m.getName())));
+
+				Flux<String> deleteServices = Flux
+					.just(this.routingEurekaServiceManifest, this.routeServiceManifest)
+					.map(this.applicationNames::get)
+					.flatMap(name -> cloudfoundry
+						.services()
+						.deleteInstance(DeleteServiceInstanceRequest.builder().name(name).build())
+						.onErrorResume(IllegalArgumentException.class, ex -> {
+								error(String.format("can't delete service %s", name), ex);
+								return Mono.empty();
+						})
+						.then(Mono.just(name)))
+					.doOnComplete(() -> Stream
+						.of(this.routingEurekaServiceManifest)
+						.map(this.manifests::get)
+						.forEach(m -> log.info("deleted service " + m.getName())));
+
+				Flux<String> pushAndCreateEurekaBackingService = Flux
+					.just((this.routingEurekaServiceManifest))
+					.map(this.manifests::get)
+					.flatMap(cloudFoundryService::pushApplication)
+					.flatMap(cloudFoundryService::createBackingService)
+					.doOnComplete(() -> Stream.of(this.routingEurekaServiceManifest)
+						.map(this.manifests::get)
+						.forEach(m -> log.info("pushed and created backing service for " + m.getName())));
+
+				Flux<String> pushApplications = Flux
+					.just(this.routeServiceManifest, this.downstreamServiceManifest)
+					.map(this.manifests::get)
+					.flatMap(cloudFoundryService::pushApplication)
+					.doOnComplete(() -> Stream
+						.of(this.routeServiceManifest, this.downstreamServiceManifest)
+						.map(this.manifests::get)
+						.forEach(m -> log.info("pushed " + m.getName()))
+					);
+
+				Flux<String> createBackingRouteService = cloudFoundryService
+					.urlForApplication(routeServiceAppName, true)
+					.flatMapMany(url ->
+						cloudfoundry
+							.services()
+							.createUserProvidedInstance(
+								CreateUserProvidedServiceInstanceRequest
+									.builder()
+									.name(routeServiceAppName)
+									.routeServiceUrl(url)
+									.build()
+							)
+							.thenMany(Mono.just(routeServiceAppName))
+					)
+					.doOnError(ex -> error("something went wrong in creating the backing route service " + routeServiceAppName, ex))
+					.doOnComplete(() -> log.info("CUPS for " + routeServiceAppName));
+
+				Flux<String> bindRouteService = cloudFoundryService
+					.findRoutesForApplication(downstreamServiceAppName)
+					.flatMap(route ->
+						cloudfoundry
+							.services()
+							.bindRoute(BindRouteServiceInstanceRequest
+								.builder()
+								.serviceInstanceName(routeServiceAppName)
+								.domainName(route.getDomain())
+								.hostname(route.getHost())
+								.path(route.getPath())
+								.build())
+							.thenMany(Mono.just(routeServiceAppName))
+					)
+					.doOnError(ex -> error("something went wrong in binding the route service " + routeServiceAppName, ex))
+					.doOnComplete(() -> log.info("bound route service " + routeServiceAppName));
+
+				Mono<Boolean> deleteOrphanedRoutes =
+					cloudfoundry
+						.routes()
+						.deleteOrphanedRoutes(DeleteOrphanedRoutesRequest.builder().build())
+						.then(Mono.just(true));
+
+				return Flux
+					.from(unbindRouteService)
+					.thenMany(deleteApplications)
+					.thenMany(deleteOrphanedRoutes)
+					.thenMany(deleteServices)
+					.thenMany(pushAndCreateEurekaBackingService)
+					.thenMany(pushApplications)
+					.thenMany(createBackingRouteService)
+					.thenMany(bindRouteService)
+					.doOnComplete(() -> log.info("..done!"))
+					.doOnError(e -> error("could not reset and deploy the applications and services", e));
+		}
+}
